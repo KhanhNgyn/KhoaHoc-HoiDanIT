b70faed8e5ba4e6fdab929050b1ce951a3c4a00e
NguyenDoKhanh <khanhndhe182074@fpt.edu.vn>
2025-12-31 01:50:05 +0700

    chapter-3

---
+++ js-ts-hoidanIT/src/video 19/video19.js
@@ -0,0 +1,16 @@
+
+
+console.log("video 19")
+
+//object: key-value
+const person = {
+    name: "khanh",
+    age: 21,
+    address: "hanoi"
+}
+
+console.log(person)
+
+//array
+const scores = [10, 3, 4, 8, 9]
+console.log(scores)
